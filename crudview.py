219bd67bd3a7b48348b3a2bf9205f3b1999529af
oamankwa <108134334+oamankwa@users.noreply.github.com>
2023-07-31 10:05:06 -0400

    Update crudview.py 07/31/2023

--- crudview.py
+++ crudview.py
@@ -1,7 +1,8 @@
+# This file is used to test the crud object
 import mysql.connector
 from crud import CreateCrud
 
-mydb = mysql.connector.connect(host="sql9.freemysqlhosting.net", user="sql9634058", passwd="VpWiQ5vKCR", database="sql9634058")
+mydb = mysql.connector.connect(host="Your host", user="Your username", passwd="Your password", database="Your database name")
 mycursor = mydb.cursor()
 
 
@@ -12,10 +13,10 @@ mycrud = CreateCrud(mydict, mylen, mylist)
 
 mycolumns = ['user_name', 'email']
 
-#mycrud.update_one('users', 'user_name', 'SamDua', 'user_id', 3, mycursor, mydb)
-# getfields = mycrud.get_fields('users', mycolumns, 'user_id', 2, mycursor)
-# for i in getfields:
-#     print(i)
+mycrud.update_one('users', 'user_name', 'SamDua', 'user_id', 3, mycursor, mydb)
+getfields = mycrud.get_fields('users', mycolumns, 'user_id', 2, mycursor)
+for i in getfields:
+    print(i)
 col = ['user_name', 'email']
 row = ['Joe Simple', 'jsimple@gmail.com']
 mycrud.update_fields('users', col, row, 'user_id', 4, mycursor, mydb )
@@ -25,19 +26,15 @@ getall = mycrud.get_all('users', mycursor)
 for i in getall:
     print(i)
 
+getone = mycrud.get_one('users', 'user_id', 1, mycursor)
+print(getone)  
+onelist = ['user_id', 'address', 'city', 'state']
+mycrud.add_csv('locations','your path/addcsv.csv', onelist, mycursor, mydb)
+mycrud.add('users', mycursor, mydb)
+
+mycrud.add_many('users', mycursor, mydb)
 
 
-
-
-#getone = mycrud.get_one('users', 'user_id', 1, mycursor)
-#print(getone)  
-#onelist = ['user_id', 'address', 'city', 'state']
-#mycrud.add_csv('locations','/home/edward/Project-Crud/Crud/addcsv.csv', onelist, mycursor, mydb)
-#mycrud.add('users', mycursor, mydb)
-
-#mycrud.add_many('users', mycursor, mydb)
-
-#
 
 
 
